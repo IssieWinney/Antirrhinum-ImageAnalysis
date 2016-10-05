7750b937bf810b965bdc008af047b85aa8b5aa2e
IssieWinney <isabel.winney@gmail.com>
2016-10-05 15:28:02 +0200

    Added a zoomed out map to show where Bages is in France, following Caroline's recommendation.

--- BagesMap/BagesMap.R
+++ BagesMap/BagesMap.R
@@ -6,6 +6,8 @@
 # Load libraries:
 library(ggmap)
 library(ggplot2)
+library(gridExtra)
+library(grid)
 
 # Load lat/longs:
 latlong <- read.table("Data/BagesLatLong-20160928.txt", header=T)
@@ -30,16 +32,43 @@ BagesMap <- ggmap(BagesMap) +
                     name = "Site") +
   xlab("Longitude") +
   ylab("Lattitude") +
-  ggtitle("Map of Bages study area")
+  ggtitle("Bages sites")
   
 
 BagesMap
 
+# following Caroline's suggestion on 5/10/2016, add a map of the general
+# area in the South of France that covers the field site:
+
+SudMap <- get_map(location = c(lon = 2, lat = 47), zoom = 6)
+
+# Take a look
+ggmap(SudMap)
+
+# Add a point for Bages
+SudMap <- ggmap(SudMap) + 
+  geom_point(aes(x=2.98, y=43.11), 
+             pch = 16, cex = 5, color = "orange") +
+  geom_point(aes(x=2.98, y=43.11), 
+             pch = 4, cex = 4) +
+  xlab("Longitude") +
+  ylab("Lattitude") +
+  ggtitle("Location of Bages")
+
+
+SudMap
 
 # export map
 
+maps <- arrangeGrob(grobs = list(SudMap, BagesMap), 
+                    ncol=2, layout_matrix = rbind(c(1,1,1,1,1,
+                                                    2,2,2,2,2,2)))
+
+grid.newpage()
+grid.draw(maps)
+
 ggsave("Plots/BagesStudyArea.pdf",
-       plot = BagesMap,
+       plot = maps,
        width = 20,
-       height = 20,
+       height = 10,
        units="cm")
